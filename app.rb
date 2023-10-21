d99b2614860c837468725464dfe158a2a2047795
vmancode <146589771+vmancode@users.noreply.github.com>
2023-10-21 19:53:31 +0000

    commit1

--- app.rb
+++ app.rb
@@ -5,50 +5,22 @@ get("/") do
   erb(:homepage)
 end
 
-get("/dice/2/6") do
+
+
+get("/dice/:dicenum/:sidenum") do
+
+  pp params
+
+  @num_dice = params.fetch("dicenum").to_i
+  @num_side = params.fetch("sidenum").to_i
+
   @rolls = []
 
-  2.times do
-    die = rand(1..6)
+  @num_dice.times do
+    die = rand(1..@num_side)
 
     @rolls.push(die)
   end
 
-  erb(:two_six)
-end
-
-get("/dice/2/10") do
-  @rolls = []
-
-  2.times do
-    die = rand(1..10)
-
-    @rolls.push(die)
-  end
-
-  erb(:two_ten)
-end
-
-get("/dice/1/20") do
-  @rolls = []
-
-  1.times do
-    die = rand(1..20)
-
-    @rolls.push(die)
-  end
-
-  erb(:one_twenty)
-end
-
-get("/dice/5/4") do
-  @rolls = []
-
-  5.times do
-    die = rand(1..4)
-
-    @rolls.push(die)
-  end
-
-  erb(:five_four)
+  erb(:flexible)
 end
